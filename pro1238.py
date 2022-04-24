3b2b7340c9c5213e8bc35e0e0096769befea4c83
tunapro <tunagul54@gmail.com>
2022-04-24 23:20:21 +0300

    init

---
+++ pro1238.py
@@ -0,0 +1,4 @@
+from src import _main
+
+if __name__ == "__main__":
+    _main()
---
+++ src/main.py
@@ -0,0 +1,33 @@
+import json
+
+"""
+Merhabaaa...
+sınav senemdeyim ve sınava yaklaşık 55 gün kaldı
+    ben de kafayı yemeye başladım
+
+ne yapacağıma dair planlama yapacağım
+
+bayadır yazılım yapmıyorum
+
+database sistemi klasör ve json bazlı olacak her bir klasörün kendine ait 
+properties.json tarzında bir dosyaası olacak. properties.json dosyasının içinde 
+tam olarak ne olacağını bilmiyorum, ama versiyon kontrolü çok önemli.
+
+Versiyon kontrolü için aklımda ufak bir fikir var. Her kod yenilediğimde versiyon
+değiştirmekle uğraşmamak için dosyaların hashini aldırmayı planlıyorum.
+
+dosya ayrımı ders bazında yapılacak her bir ders json dosyasının içinde de tarih
+bazında sıralama yapılacak.
+
+keyword bellirlemeliyim
+
+
+"""
+
+
+def parser():
+    pass
+
+def _main():
+    print("Sanity Check.")
+
