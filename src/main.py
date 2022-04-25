024eca02a1d8f21c8a8bf9496d14024454b3646e
tunapro <tunagul54@gmail.com>
2022-04-25 15:50:36 +0300

    açıkçası commit sistemini sadece bilgisyarı kapatmadan önce kaydettiğime emin olmak için kullanıyorum

--- src/main.py
+++ src/main.py
@@ -20,40 +20,48 @@ import json
 # dosya ayrımı ders bazında yapılacak her bir ders json dosyasının içinde de tarih
 # bazında sıralama yapılacak.
 # 
-# keyword bellirlemeliyim
+# keyword belirlemeliyim
 #	reconfigure
+#	fetch
 #	help
+#
+#	list 
+#		subject
+#		data
 # 
 #	add 
-#	data
-#	
+#		subject
+#		data
+#
 #	remove
-#	data
-# 
+#		subject
+#		data
+#
+# şimdilik bu keywordler yeterli
 #
 
 
 
 
 
+def _help(): return "ok"
 
-def parser():
-	input(">>> ")
-
+def parser(text):
+	
+	try:
+		print(exec("_" + text.strip().replace(" ", "_")))
+	except:
+		print("fuck")
 
 def completer(text, state):
 	words = readline.get_line_buffer().split(" ")
 	vocab = []
 
 	if len(words) == 1:
-		vocab = ["tuna", "aslı", "memedali"]
-	elif len(words) == 2:
-		if words[0] == "tuna":
-			vocab = ["gül", "akkaya", "akyüz"]
-		elif words[0] == "aslı":
-			vocab = ["özler", "özlemez"]
-		elif words[0] == "memedali":
-			vocab = ["uçar", "uçamaz", "balon"]
+		vocab = ["help", "reconfigure", "fetch", "list", "add", "remove"]
+
+	elif len(words) == 2 and words[0] in ["list", "add", "remove"]:
+		vocab = ["subject", "data"]
 		
 	results = [i for i in vocab if i.startswith(text)] + [None]
 	return results[state]
@@ -62,7 +70,7 @@ def completer(text, state):
 def _main():
 	readline.parse_and_bind("tab: complete")
 	readline.set_completer(completer)
-	parser()
+	parser(input(">>> "))
 
 
 
